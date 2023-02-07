5bbb57ebd61f6e44f75aaa5ccd6e470582b3375c
awsalvin <awsalvin@amazon.com>
2023-02-07 00:26:43 -0600

    initial working commit

--- lib/cdk-amplify-console-stack.ts
+++ lib/cdk-amplify-console-stack.ts
@@ -1,42 +1,65 @@
 import { Stack, StackProps, SecretValue } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as amplify from 'aws-cdk-lib/aws-amplify';
+//import {App} from 'aws-cdk-lib/aws-amplify';
+import {App, GitHubSourceCodeProvider, BasicAuth} from '@aws-cdk/aws-amplify-alpha'
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
 
 export class AwsCdkAmplifyConsoleStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const amplifyApp = new amplify.App(this, 'AmplifyConsoleDemo', {
-      sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
-        owner: 'nikovirtala',
-        repository: 'amplify-console-demo',
-        oauthToken: SecretValue.secretsManager('github-access-token'),
+    const amplifyApp = new App(this, 'AmplifyConsoleDemo', {
+
+      sourceCodeProvider: new GitHubSourceCodeProvider({
+        owner: 'vaughngit',
+        repository: 'amplify-car-rental-app',
+        oauthToken: SecretValue.secretsManager('dev/amplify/github-access-token'),
       }),
-      autoBranchCreation: {
-        patterns: ['*'],
-        basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
-      },
+      // autoBranchCreation: {
+      //   patterns: ['*'],
+      //   basicAuth: BasicAuth.fromGeneratedPassword('username'),
+      // },
       autoBranchDeletion: true,
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: '1.0',
-        frontend: {
-          artifacts: {
-            baseDirectory: '/',
-            files: '**/*',
-          },
-        },
-      }),
+      buildSpec: codebuild.BuildSpec.fromObject(
+        {
+          version: 1,
+          frontend: {
+            phases: {
+              preBuild: {
+                commands: [
+                  "npm ci"
+                ]
+              },
+              build: {
+                commands: [
+                  "npm run build"
+                ]
+              }
+            },
+            artifacts: {
+              baseDirectory: "build",
+              files: [
+                "**/*"
+              ]
+            },
+            cache: {
+              paths: [
+                "node_modules/**/*"
+              ]
+            }
+          }
+        }        
+      ),
     });
 
-    const master = amplifyApp.addBranch('master');
+    const main = amplifyApp.addBranch('main');
     const dev = amplifyApp.addBranch('dev', {
-      basicAuth: amplify.BasicAuth.fromGeneratedPassword('username'),
+      basicAuth: BasicAuth.fromGeneratedPassword('username'),
     });
 
     const domain = amplifyApp.addDomain('amplified.host');
-    domain.mapRoot(master);
-    domain.mapSubDomain(master, 'www');
+    domain.mapRoot(main);
+    domain.mapSubDomain(main, 'www');
     domain.mapSubDomain(dev);
   }
 }
