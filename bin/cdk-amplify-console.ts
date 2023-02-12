797f1e23e6760a193c1016af242f515645956189
VaughnGit <alvin@vaughns.net>
2023-02-11 22:58:41 -0600

    specified role, appname, desc, and tags

--- bin/cdk-amplify-console.ts
+++ bin/cdk-amplify-console.ts
@@ -4,17 +4,22 @@ import * as cdk from 'aws-cdk-lib';
 import { AwsCdkAmplifyConsoleStack } from '../lib/cdk-amplify-console-stack';
 import { GuestUserStack } from '../lib/guest-user-backend-stack'
 
+const aws_region = 'us-east-2'
+const environment = 'dev'
+const solutionName = "tnc"
+const costcenter = "tnc-helloworld-amplify"
+
 const app = new cdk.App();
 new AwsCdkAmplifyConsoleStack(app, 'AwsCdkAmplifyConsoleStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: "us-east-2",
-  },
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: aws_region || process.env.AWS_Region },
+  environment, 
+  solutionName, 
+  costcenter,
 });
 
-new GuestUserStack(app, 'AmplifyCognitoBackendStack', {
-  env: {
-    account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: "us-east-2",
-  },
-})
+// new GuestUserStack(app, 'AmplifyCognitoBackendStack', {
+//   env: {
+//     account: process.env.CDK_DEFAULT_ACCOUNT,
+//     region: "us-east-2",
+//   },
+// })
--- lib/cdk-amplify-console-stack.ts
+++ lib/cdk-amplify-console-stack.ts
@@ -1,15 +1,38 @@
-import { Stack, StackProps, SecretValue } from 'aws-cdk-lib';
+import { Stack, StackProps, SecretValue, Tags } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 //import {App} from 'aws-cdk-lib/aws-amplify';
 import {App, GitHubSourceCodeProvider, BasicAuth} from '@aws-cdk/aws-amplify-alpha'
 import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import { ManagedPolicy, Role, ServicePrincipal } from 'aws-cdk-lib/aws-iam';
+
+export interface IStackProps extends StackProps{
+  environment: string; 
+  costcenter: string; 
+  solutionName: string; 
+}
+
 
 export class AwsCdkAmplifyConsoleStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: IStackProps) {
     super(scope, id, props);
+
+    const amplifyAppServiceRole =  new Role(this, 'AmplifyAppRole', {
+      roleName: `${props?.solutionName}-amplifyApp-${props?.environment}-${this.region}`,
+      assumedBy: new ServicePrincipal('amplify.amazonaws.com'),
+      description: 'Amplify App Service Role',
+      //inlinePolicies: {codeBuildProjectRoleInlinePolicy},
+      managedPolicies: 
+      [
+        ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess-Amplify')
+      ]
+    })
+  
 
     const amplifyApp = new App(this, 'AmplifyConsoleDemo', {
 
+      appName: "HelloWorldApp",
+      description: "Example App to Development with Amplify",
+      role: amplifyAppServiceRole, 
       sourceCodeProvider: new GitHubSourceCodeProvider({
         owner: 'vaughngit',
         repository: 'amplify-car-rental-app',
@@ -18,17 +41,27 @@ export class AwsCdkAmplifyConsoleStack extends Stack {
       autoBranchCreation: {
         patterns: ['*'],
       //  patterns: ['feature/*', 'test/*'],
-      //  basicAuth: BasicAuth.fromGeneratedPassword('username'),
+        basicAuth: BasicAuth.fromGeneratedPassword('clouditadmin'),
       },
       autoBranchDeletion: true,
       buildSpec: codebuild.BuildSpec.fromObject(
         {
           version: 1,
+          backend: {
+            phases: {
+              preBuild:{
+                commands: [
+                 // "amplifyPush --simple"
+                ]
+              }
+            }
+          },
           frontend: {
             phases: {
               preBuild: {
                 commands: [
-                  "npm ci"
+                  "npm ci",
+                  "amplifyPush --simple",
                 ]
               },
               build: {
@@ -53,14 +86,22 @@ export class AwsCdkAmplifyConsoleStack extends Stack {
       ),
     });
 
-    const main = amplifyApp.addBranch('main');
+    
+
+    const main = amplifyApp.addBranch('main', {
+      basicAuth: BasicAuth.fromGeneratedPassword('clouditadmin'),
+    });
     const dev = amplifyApp.addBranch('dev', {
-      basicAuth: BasicAuth.fromGeneratedPassword('username'),
+      basicAuth: BasicAuth.fromGeneratedPassword('clouditadmin'),
     });
 
-    const domain = amplifyApp.addDomain('amplified.host');
+    const domain = amplifyApp.addDomain('thehandle.com');
     domain.mapRoot(main);
     domain.mapSubDomain(main, 'www');
     domain.mapSubDomain(dev);
+
+    Tags.of(this).add("environment", props.environment)
+    Tags.of(this).add("solution", props.solutionName)
+    Tags.of(this).add("costcenter", props.costcenter)
   }
 }
